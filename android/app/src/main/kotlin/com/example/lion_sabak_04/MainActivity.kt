4257d754c8e93ce2dcf4b40728be17bbcc45baf4
azackmatoff <ackmatoff@gmail.com>
2024-04-12 23:16:24 +0600

    riverpod added

---
+++ android/app/src/main/kotlin/com/example/lion_sabak_04/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lion_sabak_04
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lion_sabak_04/ui/pages/dice_page.dart';
+import 'package:lion_sabak_04/ui/pages/dice_page_with_riverpod.dart';
+
+void main() {
+  runAppWithRiverpod();
+}
+
+void runAppWithFlutter() {
+  runApp(const MyFlutterApp());
+}
+
+void runAppWithRiverpod() {
+  runApp(const ProviderScope(child: MyRiverpodApp()));
+}
+
+class MyFlutterApp extends StatelessWidget {
+  const MyFlutterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const DicePage(),
+    );
+  }
+}
+
+class MyRiverpodApp extends StatelessWidget {
+  const MyRiverpodApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const DicePageWithRiverpod(),
+    );
+  }
+}
